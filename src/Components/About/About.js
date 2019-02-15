60d1062e00c05380437dc1fb56b4bb491e727af5
ymoua17 <44180276+ymoua17@users.noreply.github.com>
2019-02-14 19:53:16 -0600

    updated to a grid

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,17 +5,20 @@ import './About.css'
 
 const About = () => {
     return (
-        <div id="about" className="ph5-ns h-auto pb4">
+        <div id="about" className="pb2">
             <h1 className="fw3 underline">About Me</h1>
-            <div className='dib'>
-                <div className="dib v-mid">
-                    <img src={portrait} alt="portrait" className="portrait br4"/>
-                </div>
-                <div className="white-80 dib w-60 f3 v-mid pa2">
-                    <p>Welcome to my portfolio! I am a Full-Stack Developer seeking a full-time employment.
+            <div className='center ph3-m mw9'>
+                <div className="cf ph2-m">
+                    <div className="v-mid pa2 fl w-100 w-50-ns">
+                        <img src={portrait} alt="portrait" className="portrait br4"/>
+                    </div>
+                    <div className="white-80 dv-mid pa3 fl w-100 w-50-ns">
+                        <p className="mt0">Welcome to my portfolio! I am a Software Developer seeking a full-time employment.
                     I have been teaching myself the fundementals of web development, both front and back-end. 
                     I have built several projects that showcases my skills and growth as a developer.</p>
+                    </div>
                 </div>
+                
             </div>
         </div>
     )
--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -4,14 +4,16 @@ import './Portfolio.css';
 class Portfolio extends React.Component {
     render() {
         return (
-            <div className="bt bb b--white-80 white-80 center">
+            <div className="bt bb b--white-80 white-80 center pb4">
                 <a className="white-80 f4 hover-bg-dark-gray dib pt3" href="https://github.com/ymoua17" target="blank">{this.props.name}</a>
-                <div className="flex flex-wrap">
-                    <div className="dib v-mid pa3 center-m">
-                        <img src="http://www.illuminationworksllc.com/wp-content/uploads/2017/04/ProjectManagement-1.jpg" alt={this.props.name} className="project ba b-white--80 br4" />
-                    </div>
-                    <div className="dib v-mid pa3 center">
-                        <p className="mt0">{this.props.tech}</p>
+                <div className="center ph3-ns mw9">
+                    <div className="cf ph2-ns">
+                        <div className="v-mid pa2 fl w-100 w-50-ns">
+                            <img src="http://www.illuminationworksllc.com/wp-content/uploads/2017/04/ProjectManagement-1.jpg" alt={this.props.name} className="project ba b-white--80 br4" />
+                        </div>
+                        <div className="white-80 v-mid pa2 fl w-100 w-50-ns">
+                            <p className="mt0">{this.props.tech} fdsjfldsfkdsjflsd jkflsdj klsdjlksd</p>
+                        </div>
                     </div>
                 </div>
             </div>
