11c39367a7b42461430eeb83f68c269e9f2d71ec
ymoua17 <44180276+ymoua17@users.noreply.github.com>
2019-04-08 17:51:47 -0500

    Updates

---
+++ precache-manifest.bf6a8771ead81ca75d3c1e735f63dbe4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bd68893575328f8e34583116dc23582",
+    "url": "/portfolio/static/media/portrait.2bd68893.jpg"
+  },
+  {
+    "revision": "a6f32879726b15a0de0d",
+    "url": "/portfolio/static/css/main.8182354c.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "436d68a78833491f1a7b6b7024e6cb97",
+    "url": "/portfolio/static/media/BreakingBadQuoteGenerator.436d68a7.gif"
+  },
+  {
+    "revision": "cb9f3933f4da0db14409",
+    "url": "/portfolio/static/js/2.cb9f3933.chunk.js"
+  },
+  {
+    "revision": "a6f32879726b15a0de0d",
+    "url": "/portfolio/static/js/main.a6f32879.chunk.js"
+  },
+  {
+    "revision": "968ea62882943e88bbd318ae5fa67429",
+    "url": "/portfolio/static/media/linkedin.968ea628.png"
+  },
+  {
+    "revision": "5ae28da96e2f87270245cb80f25e372c",
+    "url": "/portfolio/static/media/gmail.5ae28da9.png"
+  },
+  {
+    "revision": "87dbc19574b046fe97251ecdf5edcbb9",
+    "url": "/portfolio/static/media/ColorGradientGenerator.87dbc195.gif"
+  },
+  {
+    "revision": "cb9f3933f4da0db14409",
+    "url": "/portfolio/static/css/2.e1b8a09f.chunk.css"
+  },
+  {
+    "revision": "c3eaf97279f9be06764ae66db656c691",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1e9e56926066fcf293f98d5ddd345274.js"
+  "/portfolio/precache-manifest.bf6a8771ead81ca75d3c1e735f63dbe4.js"
 );
 
 workbox.clientsClaim();
