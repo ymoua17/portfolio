40a3e9dabad1957aef829ed7f26b990670933f56
ymoua17 <44180276+ymoua17@users.noreply.github.com>
2019-05-10 18:01:56 -0500

    Updates

---
+++ precache-manifest.9089ddf7dc19208421821cc48dab557a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48644f98bd5640d65d2ecc1a5f3efda6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "756794e82169d237f554",
+    "url": "/portfolio/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "59d7fa8139698a43d0c6",
+    "url": "/portfolio/static/css/main.5b0a1eed.chunk.css"
+  },
+  {
+    "revision": "756794e82169d237f554",
+    "url": "/portfolio/static/js/2.c2b54028.chunk.js"
+  },
+  {
+    "revision": "59d7fa8139698a43d0c6",
+    "url": "/portfolio/static/js/main.26f4f4f4.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "436d68a78833491f1a7b6b7024e6cb97",
+    "url": "/portfolio/static/media/BreakingBadQuoteGenerator.436d68a7.gif"
+  },
+  {
+    "revision": "87dbc19574b046fe97251ecdf5edcbb9",
+    "url": "/portfolio/static/media/ColorGradientGenerator.87dbc195.gif"
+  },
+  {
+    "revision": "c2c9e1fc63a2e0dd803cd30e513c1707",
+    "url": "/portfolio/static/media/JavascriptCalculator.c2c9e1fc.gif"
+  },
+  {
+    "revision": "5ae28da96e2f87270245cb80f25e372c",
+    "url": "/portfolio/static/media/gmail.5ae28da9.png"
+  },
+  {
+    "revision": "968ea62882943e88bbd318ae5fa67429",
+    "url": "/portfolio/static/media/linkedin.968ea628.png"
+  },
+  {
+    "revision": "2bd68893575328f8e34583116dc23582",
+    "url": "/portfolio/static/media/portrait.2bd68893.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.5927826c6baa4ecc18294fa32511998d.js"
+  "/portfolio/precache-manifest.9089ddf7dc19208421821cc48dab557a.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
